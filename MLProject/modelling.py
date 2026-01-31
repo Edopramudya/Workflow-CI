1d520a2767aeed179b5afc852b1de38365293c80
Edo Pramudya <edopramudya174@gmail.com>
2026-01-31 17:03:54 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,1 +1,36 @@
+import numpy as np
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+X = np.load(os.path.join(BASE_DIR, "X.npy"))
+y = np.load(os.path.join(BASE_DIR, "y.npy"))
+
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Set MLflow experiment
+mlflow.set_experiment("Titanic_Basic_Experiment")
+
+# Enable autologging
+mlflow.autolog()
+
+# Start MLflow run
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Accuracy:", acc)
 
